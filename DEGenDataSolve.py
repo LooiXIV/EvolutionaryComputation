d184350d80ab39adbe1bf0efe6197ff720b7ceef
Alexander Looi <LooiXIV@gmail.com>
2018-11-30 23:53:26 -0500

    created parameters from the generated data

--- DEGenDataSolve.py
+++ DEGenDataSolve.py
@@ -38,9 +38,7 @@ for sig in Sigmas:
 
         t1 = timer.clock()
         
-        #solutions = DE(fm.FitnessFuncSO, bounds, popsize=100, maxiter=100,
-        #               args=(y0, time, chData, False, True))
-        solutions = DE(fm.FitnessFuncSO, bounds, popsize=10, maxiter=10,
+        solutions = DE(fm.FitnessFuncSO, bounds, popsize=100, maxiter=100,
                        args=(y0, time, chData, False, True))
         t2 = timer.clock()
         SigDESol[sig][seedNum] = solutions
@@ -48,6 +46,6 @@ for sig in Sigmas:
         print("Elapsed Time (seconds): "+str(t2-t1))
     print("~"*60)
 
-with open('SalDESolutions.pkl', 'wb') as outfile:
+with open('Data/SigDESolutions.pkl', 'wb') as outfile:
     pkl.dump(SigDESol, outfile)
 
